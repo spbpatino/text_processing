8d2e0bfb74ca193028b1ea90625b29b0f4224947
santiago patiño betancur <spb.patino@MacBook-Air-de-santiago.local>
2017-07-08 18:12:57 -0500

    probando la librería textblob

--- text_classification_1.py
+++ text_classification_1.py
@@ -1,1 +1,41 @@
 #ALL CODE HERE
+
+from textblob.classifiers import NaiveBayesClassifier
+
+train = [
+    ('I love this sandwich.', 'pos'),
+    ('This is an amazing place!', 'pos'),
+    ('I feel very good about these beers.', 'pos'),
+    ('This is my best work.', 'pos'),
+    ("What an awesome view", 'pos'),
+    ('I do not like this restaurant', 'neg'),
+    ('I am tired of this stuff.', 'neg'),
+    ("I can't deal with this", 'neg'),
+    ('He is my sworn enemy!', 'neg'),
+    ('My boss is horrible.', 'neg')
+]
+test = [
+    ('The beer was good.', 'pos'),
+    ('I do not enjoy my job', 'neg'),
+    ("I ain't feeling dandy today.", 'neg'),
+    ("I feel amazing!", 'pos'),
+    ('Gary is a friend of mine.', 'pos'),
+    ("I can't believe I'm doing this.", 'neg')
+]
+
+cl = NaiveBayesClassifier(train)
+
+#print(cl)
+
+#print(cl.classify("Their burgers are amazing"))
+#print(cl.classify("I don't like their pizza."))
+
+from textblob import TextBlob
+blob = TextBlob("The beer was amazing. "
+                "But the hangover was horrible. My boss was not happy.",
+                classifier=cl)
+
+print(blob.sentences)
+print(blob.classify())
+
+print(cl.accuracy(test))
